439d0b15f32e3b43bc085e4e942d32ec0c0b2a84
Mykola Shapka <shmm@ukr.net>
2022-03-20 20:01:34 +0100

    MainExtended

---
+++ codilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Mykola";
+        String myAddress = "Warszawa";
+        int buildN = 38;
+        double postCode = 03.287;
+        boolean prawoJazdy = true;
+        char kat = 'B';
+        System.out.println (postCode);
+    }
+}
