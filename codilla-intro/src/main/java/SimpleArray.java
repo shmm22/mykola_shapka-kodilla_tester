1a4c393d1466dad8a8b741cc9d7110e7c2f34882
Mykola Shapka <shmm@ukr.net>
2022-04-03 19:01:39 +0200

    simplearray

---
+++ codilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,11 @@
+public class SimpleArray {
+    public static void main (String[] args){
+        String [] name = new String[5];
+        name[0]="Max";
+        name[1]="Margaryta";
+        name[2]="Svitlana";
+        name[3]="Molly";
+        name[4]="Natalia";
+       String name = name[3];
+    }
+}
