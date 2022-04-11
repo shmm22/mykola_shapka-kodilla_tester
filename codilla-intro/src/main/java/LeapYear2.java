710a361ff52ac5e260ec9c0db8ff20aad23d4730
Mykola Shapka <shmm@ukr.net>
2022-04-11 21:37:56 +0200

    LeapYear

---
+++ codilla-intro/src/main/java/LeapYear2.java
@@ -0,0 +1,14 @@
+public class LeapYear2 {
+    public static void main (String[] args){
+int year = 2020;
+System.out.println("Year:" + year);
+boolean czyPrzystepny;
+if (year % 4 == 0 || year % 100 == 0 && year % 400 == 0) {
+    czyPrzystepny = true;
+} else {
+    czyPrzystepny = false;
+}
+        System.out.println("Czy rok jest przystepny:" + czyPrzystepny);
+
+    }
+}
